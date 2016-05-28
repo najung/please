e7e371092467d96ac6a38c68c4fa548e55bb6ebe
najung <h5rse@naver.com>
2016-05-28 12:14:22 +0000

    ssangul

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
     belongs_to :user
+    
+    validates :title, presence: true
+    validates :content, presence: true
+    validates :image_url, presence: true
 end
 
